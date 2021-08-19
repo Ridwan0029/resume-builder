b3b61094cb3a80b6e1bd9aa31ed011a50a86236b
Ridwan <ridwanraji4@gmail.com>
2021-08-19 23:04:55 +0100

    creating layouts

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+var data = [
+    {firstName: 'Ridwan', lastName: 'Raji'}
+]
 
 app.get('/', function(req, res){
     res.render('landing');
 });
 
 app.get('/myresume', function(req, res){
-    var data = [
-        {firstName: 'Ridwan', lastName: 'Raji'}
-    ]
+   
 
-    res.render('myresume', {data: data});
-})
+    res.render('myresume', {data: data });
+});
+
+app.post('/myresume', function(req, res){
+    //get data from form and add to data array
+    var firstName = req.body.nameOne
+    var lastName = req.body.nameTwo
+    var user =  {firstName: firstName, lastName: lastName}
+    data.push(user);
+    res.redirect('/myresume')
+});
+
+app.get('/myresume/new', function(req, res){
+    res.render('new.ejs');
+});
 
 app.listen(3000, function(){
-    console.log('resume-bulder server has started!');
+    console.log('resume-builder server has started!');
 });
